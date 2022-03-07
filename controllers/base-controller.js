514125c9e57ac61985fd50abc6fadef07014b4d4
Lucas Campos jauregui <lvcasx1@Lucass-MacBook-Pro.local>
2022-03-06 21:51:16 -0300

    added create endpoint connection to database

--- controllers/base-controller.js
+++ controllers/base-controller.js
@@ -1,3 +1,5 @@
+const videogameModel = require('../models/videogameModel');
+
 module.exports = {
   getAll: (req, res) => {
     res.send(req.params);
@@ -6,7 +8,20 @@ module.exports = {
     res.send(req.params.id);
   },
   create: (req, res) => {
-    res.send(req.body);
+    const videogame = new videogameModel({
+      name: req.body.name,
+      year: req.body.year,
+      publisher: req.body.publisher,
+      genre: req.body.genre
+    });
+
+    videogame.save((error, doc) => {
+      if(!error) {
+        res.send('Videogame Created Successfully!');
+      } else {
+        res.status(500).send('Erorr while saving videogame on database');
+      }
+    })
   },
   updateById: (req, res) => {
     res.send(req.params.id);
