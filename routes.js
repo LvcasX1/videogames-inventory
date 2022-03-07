e333474f335ce1101c2ed8a40c3eb6ebc1f50724
Lucas Campos jauregui <lvcasx1@Lucass-MacBook-Pro.local>
2022-03-07 19:30:23 -0300

    src folder added and routes file updated

--- routes.js
+++ routes.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const router = express.Router();
 const { Validator } = require('express-json-validator-middleware');
-
-const videogamesController = require('./controllers/videogamesController');
+const videogamesController = require('./src/controllers/videogamesController');
+const videogamesSchema = require('./src/schemas/videogames-schema');
 
 const { validate } = new Validator();
-
-const videogamesSchema = require('./schemas/videogames-schema');
+const router = express.Router();
 
 router.get('/', videogamesController.getAll);
 router.get('/:id', videogamesController.getById);
