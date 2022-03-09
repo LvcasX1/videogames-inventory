df68245ac5ad930623a48926159b1a7f24640912
Lucas Campos jauregui <lvcasx1@Lucass-MacBook-Pro.local>
2022-03-08 21:12:35 -0300

    adds jest and videogameModel test

--- src/models/videogameModel.js
+++ src/models/videogameModel.js
@@ -2,10 +2,10 @@ const { Schema } = require('mongoose');
 const mongoose = require('../../database');
 
 const videogameSchema = new Schema({
-  name: String,
-  year: Number,
-  publisher: String,
-  genre: String,
+  name: { type: String, required: true },
+  year: { type: Number, required: true },
+  publisher: { type: String, required: true },
+  genre: { type: String, required: true },
 });
 
 const videogameModel = mongoose.model('videogames', videogameSchema);
---
+++ src/models/videogameModel.spec.js
@@ -0,0 +1,75 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-undef */
+const mongoose = require('mongoose');
+const VideogameModel = require('./videogameModel');
+
+const videogameMock = {
+  name: 'Persona',
+  year: 2022,
+  publisher: 'atlus',
+  genre: 'jrpg',
+};
+
+describe('Videogame Model Test', () => {
+  beforeAll(async () => {
+    await mongoose.connect(
+      process.env.DB_URL,
+      { useNewUrlParser: true, useCreateIndex: true },
+      (error) => {
+        if (error) {
+          console.error(error);
+          process.exit(1);
+        }
+      },
+    );
+  });
+
+  afterAll(async () => {
+    await mongoose.connection.close();
+  });
+
+  it('create and save a videogame', async () => {
+    const validVideogame = new VideogameModel(videogameMock);
+    const savedVideogame = await validVideogame.save();
+
+    expect(savedVideogame._id).toBeDefined();
+    expect(savedVideogame.name).toBe(videogameMock.name);
+    expect(savedVideogame.year).toBe(videogameMock.year);
+    expect(savedVideogame.publisher).toBe(videogameMock.publisher);
+    expect(savedVideogame.genre).toBe(videogameMock.genre);
+  });
+
+  it('create user and properties not defined in schema should be undefined', async () => {
+    const videogaeWithInvalidProperty = new VideogameModel(
+      {
+        name: 'Dragon Ball Z Hyper Dimension',
+        description: 'amazing fighting game',
+        year: '1997',
+        genre: 'fighting',
+        publisher: 'Bandai',
+      },
+    );
+
+    const savedVideogameWithInvalidProperty = await videogaeWithInvalidProperty.save();
+
+    expect(savedVideogameWithInvalidProperty._id).toBeDefined();
+    expect(savedVideogameWithInvalidProperty.description).toBeUndefined();
+  });
+
+  it('should fail when creating a videogame withuot required properties', async () => {
+    const videogameWithoutRequiredField = new VideogameModel({ name: 'Phantasy Star' });
+    let err;
+
+    try {
+      const savedVideogameWithoutREquiredField = await videogameWithoutRequiredField.save();
+      error = savedVideogameWithoutREquiredField;
+    } catch (error) {
+      err = error;
+    }
+
+    expect(err).toBeInstanceOf(mongoose.Error.ValidationError);
+    expect(err.errors.genre).toBeDefined();
+    expect(err.errors.year).toBeDefined();
+    expect(err.errors.publisher).toBeDefined();
+  });
+});
